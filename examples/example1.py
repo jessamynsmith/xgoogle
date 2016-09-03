1bb988db4b8ac3ac61123f6864a1f8990c784ca0
Jessamyn Smith <geekchick77@gmail.com>
2016-09-03 12:58:25 -0400

    Updated example to get results until end is reached.

--- examples/example1.py
+++ examples/example1.py
@@ -5,16 +5,16 @@
 #
 
 from xgoogle.search import GoogleSearch, SearchError
-try:
-  gs = GoogleSearch("quick and dirty")
-  gs.results_per_page = 50
-  results = gs.get_results()
-  print(results)
-  for res in results:
-    print res.title.encode('utf8')
-    print res.desc.encode('utf8')
-    print res.url.encode('utf8')
-    print
-except SearchError, e:
-  print "Search failed: %s" % e
 
+try:
+    gs = GoogleSearch("quick and dirty")
+    gs.results_per_page = 50
+    while not gs.eor:
+        results = gs.get_results()
+        for res in results:
+            print res.title.encode('utf8')
+            print res.desc.encode('utf8')
+            print res.url.encode('utf8')
+            print
+except SearchError, e:
+    print "Search failed: %s" % e
